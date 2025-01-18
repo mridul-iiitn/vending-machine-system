f40ffc6e6d06194832ae9d1eb19a4a2b9a6aa1ec
mridul-iiitn <mridulpandey619@gmail.com>
2025-01-18 12:50:50 +0530

    Update VendingMachine_TestBench.v

--- VendingMachine_TestBench.v
+++ VendingMachine_TestBench.v
@@ -1,44 +1,72 @@
-
+// Code your testbench here
+// or browse Examples
+// Predefining the clock period
 `define clock_period 10
 
+// Module for the testbench
 module VendingMachine_TestBench();
+    // Defining the inputs and the outputs
     reg [3:0] item_number;
     reg nickel_in, dime_in, clock, reset;
     wire nickel_out, dispense;
+    
+    // Calling the module 
+    VendingMachine VM(.item_number(item_number), .nickel_in(nickel_in), .dime_in(dime_in), .clock(clock), .reset(reset), .nickel_out(nickel_out), .dispense(dispense));
 
-    // Instantiate the Vending Machine
-    VendingMachine VM(
-        .item_number(item_number), 
-        .nickel_in(nickel_in), 
-        .dime_in(dime_in), 
-        .clock(clock), 
-        .reset(reset), 
-        .nickel_out(nickel_out), 
-        .dispense(dispense)
-    );
-
-    // Clock signal
+    // Intialising the clock value
     initial clock = 1;
-    always #(`clock_period/2) clock = ~clock;
 
-    // Test cases
+    // Updating clock value at a regular interval
+    always #(`clock_period/2) clock = (~clock);
+
+    // Input beginning
     initial begin
-        // Reset
-        reset = 1; item_number = 4'b0001; nickel_in = 0; dime_in = 0;
+        // Initialising the values
+        item_number = 4'b0010;
+        nickel_in = 0;
+        dime_in = 0;
+        reset = 0;
+
+        // Giving new inputs at regular intervals
+        #(`clock_period);
+        reset = 1;
+
         #(`clock_period);
         reset = 0;
 
-        // Test adding coins
-        nickel_in = 1; #(`clock_period); nickel_in = 0;
-        dime_in = 1; #(`clock_period); dime_in = 0;
+        #(`clock_period);
+        nickel_in = 1;
+        dime_in = 0;
+        
+        #(`clock_period);
+        nickel_in = 0; 
+        dime_in = 1;
 
-        // Finish
+        #(`clock_period);
+        nickel_in = 0;
+        dime_in = 0;
+        
+        #(`clock_period);
+        nickel_in = 0;
+        dime_in = 1;
+
+        #(`clock_period);
+        nickel_in = 0;
+        dime_in = 0;
+
+        #(`clock_period);
+        nickel_in = 0;
+        dime_in = 0;
+
+        #(`clock_period);
+
         #(`clock_period*5);
         $finish;
     end
 
+    // Dumping the file and variables as I am testing this on EDA playground
     initial begin
         $dumpfile("dumpfile.vcd");
-        $dumpvars;
+        $dumpvars; 
     end
 endmodule
